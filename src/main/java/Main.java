4a411d7185494f4177e8fac36ba53736035db1d6
Jack Cook <hello@jackcook.nyc>
2016-12-18 11:46:43 -0500

    Remove unused code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,6 @@
-import java.net.MalformedURLException;
-import java.net.URL;
-
 public class Main {
 
     public static void main(String[] args) {
-//        URL url;
-//
-//        try {
-//            url = new URL("http://www.lefigaro.fr/international/2016/12/17/01003-20161217ARTFIG00062-environ-40000-syriens-attendent-la-reprise-des-evacuations-d-alep.php");
-//        } catch (MalformedURLException e) {
-//            e.printStackTrace();
-//            return;
-//        }
-//
-//        ArticleParser.parseArticle(url, Language.FRENCH);
-
         ArticleFetcher.retrieveArticles(Language.FRENCH);
     }
 }
