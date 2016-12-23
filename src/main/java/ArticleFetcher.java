c6a532bedf0f2c451ca875c32da0a13308e6e16f
Jack Cook <hello@jackcook.nyc>
2016-12-22 19:56:32 -0500

    Working on SQLite integration

--- src/main/java/ArticleFetcher.java
+++ src/main/java/ArticleFetcher.java
@@ -8,10 +8,13 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.sql.*;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class ArticleFetcher {
 
@@ -53,11 +56,16 @@ public class ArticleFetcher {
         for (int i = 0; i < retrievedArticles.length(); i++) {
             JSONObject retrievedArticle = retrievedArticles.getJSONObject(i);
             String urlString = retrievedArticle.getString("url");
-            System.out.println(urlString);
 
             try {
                 URL url = new URL(urlString);
-                Article article = parseArticle(url, language);
+                Article article = lookupArticle(url);
+
+                if (article == null) {
+                    article = parseArticle(url, language);
+                    storeArticle(article);
+                }
+
                 System.out.println(article.title);
             } catch (MalformedURLException e) {
                 e.printStackTrace();
@@ -123,5 +131,111 @@ public class ArticleFetcher {
         }
 
         return new Article(articleURL, title, imageURL, content, author, publishedDate, language);
+    }
+
+    public static Article lookupArticle(URL url) {
+        Connection c = null;
+        Statement stmt = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:main.db");
+            c.setAutoCommit(false);
+
+            System.out.println("SELECT * FROM ARTICLES WHERE URL = '" + url.toString() + "';");
+
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM ARTICLES WHERE URL = '" + url.toString() + "';");
+
+            while (rs.next()) {
+                int id = rs.getInt("ID");
+                System.out.println(id);
+            }
+
+            System.out.println("Done getting stuff");
+
+            rs.close();
+            stmt.close();
+            c.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static void storeArticle(Article article) {
+        Connection c = null;
+        Statement stmt = null;
+
+        String sql = null; // up here for easy debugging
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:main.db");
+
+            List<String> keys = new ArrayList<>();
+            List<String> values = new ArrayList<>();
+
+            keys.add("URL");
+            values.add("'" + article.url.toString() + "'");
+
+            keys.add("TITLE");
+            values.add("'" + article.title.replace("'", "''") + "'");
+
+            if (article.imageURL != null) {
+                keys.add("IMAGEURL");
+                values.add("'" + article.imageURL.toString() + "'");
+            }
+
+            if (article.content != null) {
+                keys.add("CONTENT");
+                values.add("'" + article.content.replace("'", "''") + "'");
+            }
+
+            if (article.author != null) {
+                keys.add("AUTHOR");
+                values.add("'" + article.author + "'");
+            }
+
+            if (article.publishedDate != null) {
+                keys.add("PUBLISH_DATE");
+                values.add("'" + article.publishedDate.toString() + "'");
+            }
+
+            keys.add("LANGUAGE");
+            values.add("'" + article.language.getISO839_1() + "'");
+
+            String keysString = "";
+
+            for (String key : keys) {
+                keysString += key + ", ";
+            }
+
+            keysString = keysString.substring(0, keysString.length() - 2);
+
+            String valuesString = "";
+
+            for (String value : values) {
+                valuesString += value + ", ";
+            }
+
+            valuesString = valuesString.substring(0, valuesString.length() - 2);
+
+            stmt = c.createStatement();
+            sql = "INSERT INTO ARTICLES (" + keysString + ") " +
+                         "VALUES (" + valuesString + ");";
+
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Attempted statement: " + sql);
+        }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import static spark.Spark.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        createArticlesTable();
+
+        ArticleFetcher.retrieveArticles(Language.FRENCH);
+
         port(8000);
 
         get("/translate", (req, res) -> {
@@ -15,5 +24,32 @@ public class Main {
 
             return Translator.translateTerm(term, source, target);
         });
+    }
+
+    public static void createArticlesTable() {
+        Connection c = null;
+        Statement stmt = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:main.db");
+
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE IF NOT EXISTS ARTICLES" +
+                         "(ID INTEGER PRIMARY KEY    AUTOINCREMENT," +
+                         " URL                TEXT   NOT NULL," +
+                         " TITLE              TEXT   NOT NULL," +
+                         " IMAGEURL           TEXT," +
+                         " CONTENT            TEXT," +
+                         " AUTHOR             TEXT," +
+                         " PUBLISH_DATE       TEXT," +
+                         " LANGUAGE           TEXT   NOT NULL)";
+
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
